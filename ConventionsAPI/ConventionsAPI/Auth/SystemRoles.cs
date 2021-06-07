6992bab6f4e0af89cfef5e3a28940285eb52a247
Jakob Gøttrup Nielsen <jakobgoettrup@gmail.com>
2021-06-07 21:06:13 +0200

    Add typed roles

---
+++ ConventionsAPI/ConventionsAPI/Auth/SystemRoles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConventionsAPI.Auth
+{
+    public class SystemRoles
+    {
+        public const string Admin = "Admin";
+        public const string Speaker = "Speaker";
+    }
+}
